61380b35820c5904b76451e6f7963868f542053a
andy <rygh4775@gmail.com>
2019-05-22 11:29:25 +0900

    add: mail v1 API 추가

--- routes/v1/group.route.js
+++ routes/v1/group.route.js
@@ -8,7 +8,8 @@ var loginRoutes = require('./login.route'),
     eventRoutes = require('./event.route'),
     aggRoutes = require('./agg.route'),
     rewardRoutes = require('./reward.route'),
-    participationRoutes = require('./participation.route');
+    participationRoutes = require('./participation.route')
+    mailRoutes = require('./mail.route');
 
 var paramValidation = require('../../config/param-validation'),
     groupCtrl = require('../../controllers/group.controller'),
@@ -32,10 +33,12 @@ router.use('/:groupId/rewards', auth, rewardRoutes);
 
 router.use('/:groupId/aggs', auth, aggRoutes);
 
+router.use('/:groupId/mails', auth, mailRoutes);
+
 router.param('groupId', (req, res, next, id) => {
   // Get group name from groupId
   req.groupId = id;
-  req.groupName = 'JAVA-CAFE';
+  req.groupName = '자바카페';
   next();
 });
 
